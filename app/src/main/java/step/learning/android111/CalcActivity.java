b2cbbe0ef189ae44feaf043f279b0c20b17cac5d
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-03-25 19:38:47 +0200

    Initial commit

---
+++ app/src/main/java/step/learning/android111/CalcActivity.java
@@ -0,0 +1,29 @@
+package step.learning.android111;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calc);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+/*
+Д.З. Створити додаткову активність у проєкті.
+Розмітити її  для гри "хрестики-нолики"
+Додати на головному екрані посилання на дану активність.
+ */
